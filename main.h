e5e62e2120f6d15c771fa34eae578243de484b66
Tewodros <teddymekuria277@gmail.com>
2022-05-17 16:18:12 -0700

    printf group project

--- main.h
+++ main.h
@@ -1,41 +1,12 @@
 #ifndef HEADER_MAIN
 #define HEADER_MAIN
 #include <stdarg.h>
-#include <stdarg.h>
-
-/**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
- */
-typedef struct flags
-{
-int plus;
-int space;
-int hash;
-} flags_t;
-
-/**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
- */
-typedef struct printHandler
-{
-char c;
-int (*f)(va_list ap, flags_t *f);
-} ph;
 int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
 int _printf(const char *format, ...);
 char *convert_to(char representation[], unsigned int num, int base);
 int function_manager(char c, va_list arg);
-
-int get_flag(char s, flags_t *f);
 int _switch(char c, va_list arg);
 int print_character(va_list arg);
 int print_sign(va_list arg, int base);
