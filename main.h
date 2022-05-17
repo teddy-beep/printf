5bf11c7920a9c6acb4f62f740d6f0e6aa284fc6e
shizoe <71692698+shizoe@users.noreply.github.com>
2022-05-17 21:55:38 +0200

    Group project

--- main.h
+++ main.h
@@ -1,65 +1,24 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
+#ifndef our_printf
+#define our_printf
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <unistd.h>
-
 /**
-* struct storage - structure that holds information on the buffer
-* @size: holds the size to write from the buffer
-* @box: pointer to the array
-* @start: pointer to the beginning of the array, will not be modified
-*/
-typedef struct storage
+ * struct specifier - struct specifier
+ * @valid: the valid character.
+ * @f: the functions associated.
+ *
+ */
+typedef struct specifier
 {
-	int size;
-	char *box;
-	char *start;
-} mk_buffer;
-
-/**
-* struct format - a structure that holds the string to be printed and a format
-* checker function pointer
-* @format: holds the string to print
-* @f: a function pointer
-*/
-typedef struct format
-{
-	char *format;
-	mk_buffer(*f)(mk_buffer, va_list);
-} format_t;
-
-/* Essential functions */
+	char *valid;
+	int (*f)(va_list);
+} spec;
 int _printf(const char *format, ...);
-
-/* Conversion specifier functions */
-mk_buffer(*get_format(const char *format))(mk_buffer, va_list);
-mk_buffer char_fmt(mk_buffer, va_list args);
-mk_buffer str_fmt(mk_buffer, va_list args);
-mk_buffer int_fmt(mk_buffer, va_list args);
-mk_buffer rev_fmt(mk_buffer, va_list args);
-mk_buffer rot13_fmt(mk_buffer buffer, va_list args);
-mk_buffer upp_hex_fmt(mk_buffer buffer, va_list args);
-mk_buffer low_hex_fmt(mk_buffer buffer, va_list args);
-mk_buffer space_fmt(mk_buffer container, const char *format, va_list args);
-mk_buffer default_fmt(mk_buffer container, const char *format);
-mk_buffer nl_fmt(mk_buffer buff, va_list var);
-mk_buffer spc_fmt(mk_buffer buff, va_list var);
-mk_buffer binary_fmt(mk_buffer buffer, va_list args);
-mk_buffer ptr_fmt(mk_buffer buffer, va_list args);
-
-/* Helper functions */
-unsigned int _strlen(char *str);
-mk_buffer rec_digits(int, mk_buffer);
-mk_buffer create_buffer(mk_buffer);
-mk_buffer add_buff(mk_buffer buff, va_list var, const char *fmt, char custom);
-void check_null(const char *);
-char *itoa(int num, int base);
-
-int is_printable(int);
-char *cvrt_upper_hex(int i);
-mk_buffer cap_s_fmt(mk_buffer buff, va_list var);
-
-#endif /* _HOLBERTON_H_ */
+int print_c(va_list args);
+int print_s(va_list args);
+int print_d(va_list args);
+int print_i(va_list args);
+int _putchar(char c);
+int print_percent(va_list args);
+int (*get_func(char x))(va_list args);
+#endif
