55c1d2e00b80dce406484977c69e40ae8d7c0439
Tewodros <teddymekuria277@gmail.com>
2022-05-19 03:29:54 -0700

    printf group ppproject

--- _printf.c
+++ _printf.c
@@ -1,162 +1,43 @@
 #include "main.h"
 /**
- *_printf - printf
- *@format: const char pointer
- *Description: this functions implement some functions of printf
- *Return: num of characteres printed
+ * _printf - printf function
+ * @format: const char pointer
+ * Return: b_len
  */
 int _printf(const char *format, ...)
 {
-const char *string;
-int cont = 0;
-va_list arg;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-if (!format)
-return (-1);
+	register int count = 0;
 
-va_start(arg, format);
-string = format;
-
-cont = loop_format(arg, string);
-
-va_end(arg);
-return (cont);
-}
-/**
- *loop_format - loop format
- *@arg: va_list arg
- *@string: pointer from format
- *Description: This function make loop tp string pointer
- *Return: num of characteres printed
- */
-int loop_format(va_list arg, const char *string)
-{
-int i = 0, flag = 0, cont_fm = 0, cont = 0, check_per = 0;
-
-while (i < _strlen((char *)string) && *string != '\0')
-{
-char aux = string[i];
-
-if (aux == '%')
-{
-i++, flag++;
-aux = string[i];
-if (aux == '\0' && _strlen((char *)string) == 1)
-return (-1);
-if (aux == '\0')
-return (cont);
-if (aux == '%')
-{
-flag++;
-}
-else
-{
-cont_fm = function_manager(aux, arg);
-if (cont_fm >= 0 && cont_fm != -1)
-{
-i++;
-aux = string[i];
-if (aux == '%')
-flag--;
-cont = cont + cont_fm;
-}
-else if (cont_fm == -1 && aux != '\n')
-{
-cont += _putchar('%');
-}
-}
-}
-check_per = check_percent(&flag, aux);
-cont += check_per;
-if (check_per == 0 && aux != '\0' && aux != '%')
-cont += _putchar(aux), i++;
-check_per = 0;
-}
-return (cont);
-}
-/**
- *check_percent - call function manager
- *@flag: value by reference
- *@aux: character
- *Description: This function print % pear
- *Return: 1 if % is printed
- */
-int check_percent(int *flag, char aux)
-{
-int tmp_flag;
-int cont = 0;
-
-tmp_flag = *flag;
-if (tmp_flag == 2 && aux == '%')
-{
-_putchar('%');
-tmp_flag = 0;
-cont = 1;
-}
-return (cont);
-}
-
-/**
- *call_function_manager - call function manager
- *@aux: character parameter
- *@arg: va_list arg
- *Description: This function call function manager
- *Return: num of characteres printed
- */
-
-int call_function_manager(char aux, va_list arg)
-{
-int cont = 0;
-
-<<<<<<< HEAD
-cont = function_manager(aux, arg);
-return (cont);
-#include <unistd.h>
-#include "main.h"
-/**
- *_printf - takes in a string and prints different types of arguments for
- * an unspecified amount of arguments
- * @format: the initial string that tell us what is going to be printed
- * Return: the amount of times we write to stdout
- */
-int _printf(const char *format, ...)
-{
-int i, count;
-
-int (*f)(va_list);
-
-va_list list;
-
-if (format == NULL)
-return (-1);
-
-va_start(list, format);
-i = count = 0;
-
-while (format[i] != '\0')
-{
-if (format[i] == '%')
-{
-if (format[i + 1] == '\0')
-return (-1);
-f = get_func(format[i + 1]);
-if (f == NULL)
-count += print_nan(format[i], format[i + 1]);
-else
-count += f(list);
-i++;
-}
-else
-{
-_putchar(format[i]);
-count++;
-}
-i++;
-}
-va_end(list);
-return (count);
-=======
-	cont = function_manager(aux, arg);
-	return (cont);
->>>>>>> 46a561218615b8cf58d5fc4aa11870d71f144199
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 }
